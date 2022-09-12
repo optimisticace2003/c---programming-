d9113caa4b4c4c5bbd9e5ed6508dcbc0c1a05145
optimisticace2003 <manikrakshan16@gmai.com>
2022-09-12 13:29:12 +0530

    12.09.2022

--- 12.09.2022.c
+++ 12.09.2022.c
@@ -4,19 +4,18 @@ int main()
 {
     int x;
     printf("enter the number  ");
-    scanf("i",&x);
-    if(x==0)
-    {
-        printf("entered number neither even or odd");
-    }
-    else if(x%2==0)
+    scanf("%d",&x);
+    
+
+   
+    if(x%2==0)
         {
-        printf("the number is  divisible by 2 /n");
+        printf("the number is  divisible by 2 \n");
         printf("the number is even /n");
         }
     else if(x%2 != 0)
         {
-        printf("the number is not divisible by 2 /n");
+        printf("the number is not divisible by 2 \n");
         printf("the number is odd /n");
         }
     
